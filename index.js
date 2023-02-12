63e54256d9e2a0545ed3cdaa6ce4b9cd5a8ca31c
Sohail Toseef <124055499+SToseef@users.noreply.github.com>
2023-02-12 14:16:03 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// First JavaScript Code!
+console.log('Hello World');
+
+let name = 'Toseef'; // String Literal
+let age = 24; // Number Literal
+let isApproved = true; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null; // Clear value of variable using null
+
+// displays variable on console
+console.log(name);
+console.log(age);
+
+// Cannot be reserved keyword
+// Variable name should be meaningful 
+// Case-sensitive
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+// on console can use typeof name to check its type
+
+let names = ['Stacy','Amy','Tracy'];
+names.push('James'); // Adds name to end of array
+// names = ['Lisa']; // reasigns array
+console.log(names);
+
+// const command forbids reassignment.
+// const is read-only variable
+// var allows variable that exists to be re-declared
